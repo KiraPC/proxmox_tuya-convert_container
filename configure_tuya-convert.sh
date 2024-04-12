e7c67253519c9d78b09f6ef1e63a02c7fec14308
Kira <46629148+KiraPC@users.noreply.github.com>
2024-04-12 11:57:13 +0200

    Update configure_tuya-convert.sh

--- configure_tuya-convert.sh
+++ configure_tuya-convert.sh
@@ -9,5 +9,8 @@ set -o pipefail #Pipe will exit with last non-zero status if applicable
 cd /root/tuya-convert
 find ./ -name \*.sh -exec sed -i -e "s/sudo \(-\S\+ \)*//" {} \;
 
+apt update
+apt install iw
+
 WLAN=$(iw dev | sed -n 's/[[:space:]]Interface \(.*\)/\1/p')
 sed -i "s/^\(WLAN=\)\(.*\)/\1$WLAN/" config.txt
